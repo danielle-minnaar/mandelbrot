4dfcf0c76037e00459158f75ed04b1d631d189a9
[Danielle Minnaar] <[danielle.minnaar@capgemini.com]>
2025-11-11 12:10:13 +0100

    Added headers to the CalcResult class.

--- Model/CalcResult.cs
+++ Model/CalcResult.cs
@@ -1,7 +1,17 @@
 namespace Mandelbrot.Model;
 
+/// <summary>
+///     A dto that holds the result of the calculations for a single point.
+/// </summary>
 public record CalcResult
 {
+    /// <summary>
+    ///     The number of iterations required to escape the bound value.
+    /// </summary>
     public required int Iterations;
+    
+    /// <summary>
+    ///     How quickly a point escapes the bound value.
+    /// </summary>
     public double? EscapeSpeed;
 }
